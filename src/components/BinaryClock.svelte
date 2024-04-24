4a68f577490721f72eafb3cf7e2882183cc328c7
Óscar Carrasco <me@oxca.be>
2024-04-24 17:46:16 +0100

    refactor: fix linting issues and formatting for BinaryClock component

--- src/components/BinaryClock.svelte
+++ src/components/BinaryClock.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-  let date = Math.trunc(Date.now() / 1000);
+let date = Math.trunc(Date.now() / 1000);
 
-  setInterval(() => {
-    date += 1;
-  }, 1000);
+setInterval(() => {
+	date += 1;
+}, 1000);
 
-  /**
-   * Turns a number `num` into a array containing its binary representation,
-   * splitted into segments of size `segmentSize`.
-   * @param num Number to process.
-   * @param segmentSize Size of the segments of the resulting array.
-   * @returns String array with the binary representation of `num` in segments
-   *  of size `segmentSize`.
-   */
-  const numberToBinarySegments = (
-    num: number,
-    segmentSize: number,
-  ): String[] => {
-    let numBinaryString = num.toString(2);
-    const regex = new RegExp(`.{1,${segmentSize}}`, "g");
+/**
+ * Turns a number `num` into a array containing its binary representation,
+ * splitted into segments of size `segmentSize`.
+ * @param num Number to process.
+ * @param segmentSize Size of the segments of the resulting array.
+ * @returns String array with the binary representation of `num` in segments
+ *  of size `segmentSize`.
+ */
+const numberToBinarySegments = (num: number, segmentSize: number): string[] => {
+	let numBinaryString = num.toString(2);
+	const regex = new RegExp(`.{1,${segmentSize}}`, "g");
 
-    const padding = "0".repeat(
-      segmentSize - (numBinaryString.length % segmentSize),
-    );
-    numBinaryString = `${padding}${numBinaryString}`;
+	const padding = "0".repeat(
+		segmentSize - (numBinaryString.length % segmentSize),
+	);
+	numBinaryString = `${padding}${numBinaryString}`;
 
-    return numBinaryString.match(regex) || [];
-  };
+	return numBinaryString.match(regex) || [];
+};
 </script>
 
-<article id="binaryclock" class="grid grid-rows-2 grid-flow-col gap-4 text-2xl text-center">
+<article
+  id="binaryclock"
+  class="grid grid-rows-2 grid-flow-col gap-4 text-2xl text-center"
+>
   {#each numberToBinarySegments(date, 4) as segment}
     <span>{segment}</span>
   {/each}
