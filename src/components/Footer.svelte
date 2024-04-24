b0cc898124fcbdb19817c1adedb5d6e6bcc6554a
Óscar Carrasco <me@oxca.be>
2024-04-24 22:38:05 +0100

    feat: add styling for links, change background color

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	let today = new Date();
+const today = new Date();
 </script>
 
-<footer class="flex justify-center">
+<footer class="flex justify-center bg-stone-400 dark:bg-black">
 	<span class="text-center">&copy; {today.getFullYear()} Óscar Carrasco. All rights reserved.</span>
 </footer>
