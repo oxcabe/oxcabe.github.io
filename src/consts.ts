b86a7be3724d29f88b125ebf254ce9932b91a7ac
Óscar Carrasco <me@oxca.be>
2024-04-24 22:03:08 +0100

    fix: add ladybird emoji, correct description structure

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 export const SITE_TITLE = 'Óscar Carrasco\'s website';
-export const SITE_DESCRIPTION = `
-Open in Ladybird browser for the best user experience.
- My website should work without JavaScript enabled.
- If that's not the case, contact me and I'll fix it.
+export const SITE_DESCRIPTION =
+  `🐞 Open in Ladybird browser for the best user experience.
+
+My website should work without JavaScript enabled. \
+If that's not the case, contact me and I'll fix it.
 `;
